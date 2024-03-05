b33810234b03b66e054a03077326b334d4351a62
HeegyuKim <heekue83@gmail.com>
2024-03-05 08:49:35 +0000

    pip install failure

--- script/setup.sh
+++ script/setup.sh
@@ -1,6 +1,7 @@
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
 pip install -U jax[tpu] -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 pip install -r qax_freeze.txt
+pip install -U flax transformers gradio
 
 # sudo apt-get install fuse gcsfuse -y
 # mkdir -p $HOME/gcs-heegyu-kogpt
